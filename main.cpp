e12d7449a6d760430f3c298d6032a8b68415664b
Goutham C M <goutham.898@gmail.com>
2021-10-26 21:13:36 +0530

    FInal commit for dijsktra shortest path

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ class Graph
     vector<int> distance;
     vector<bool> visited;
     vector<int> parent;
-
+    vector<vector<int>> result;
 public:
     Graph(int v)
     {
@@ -29,6 +29,7 @@ public:
         distance.resize(v);
         visited.resize(v);
         parent.resize(v);
+        result.resize(v);
     }
     void set_destination(int des){
         destination_final_city = des;
@@ -121,26 +122,37 @@ public:
             }
         }
         print_paths(distance, vertex, parent);
+        for(int i=0; i<result.size(); i++){
+            if(i==destination_final_city){
+                continue;
+            }else{
+                for(int j=0; j<result[i].size(); j++){
+                    cout<<result[i][j]<<" ";
+                }
+            }
+            cout<<endl;
+        }
     }
     void print_paths(vector<int> distance, int n, vector<int> parent)
     {
         for (int i = 0; i < n; i++)
         {
             if(i==destination_final_city) continue;
-            cout << destination_final_city << " ";
-            print_p(parent, i);
-            cout << endl;
+            // cout << destination_final_city << " ";
+            print_p(parent, i, i);
+            result[i].push_back(destination_final_city);
+            // cout << endl;
         }
     }
-    void print_p(vector<int> parent, int k)
+    void print_p(vector<int> parent, int k, int i)
     {
         if (parent[k] == -1)
         {
             return;
         }
-
-        print_p(parent, parent[k]);
-        cout << k << " ";
+        result[i].push_back(k);
+        print_p(parent, parent[k], i);
+        // cout << k << " ";
     }
     int min_distance(vector<int> distance, vector<bool> visited)
     {
@@ -166,23 +178,35 @@ public:
 
 int main()
 {
-    Graph g(9);
-    g.add_edge(0, 1, 4);
-    g.add_edge(0, 7, 8);
-    g.add_edge(1, 2, 8);
-    g.add_edge(1, 7, 11);
-    g.add_edge(2, 3, 7);
-    g.add_edge(2, 8, 2);
-    g.add_edge(2, 5, 4);
-    g.add_edge(3, 4, 9);
-    g.add_edge(3, 5, 14);
-    g.add_edge(4, 5, 10);
-    g.add_edge(5, 6, 2);
-    g.add_edge(6, 7, 1);
-    g.add_edge(7, 8, 7);
-    g.add_edge(6, 8, 6);
-    g.set_destination(0);
-    g.dijsktra();
+    // Graph g(9);
+    // g.add_edge(0, 1, 4);
+    // g.add_edge(0, 7, 8);
+    // g.add_edge(1, 2, 8);
+    // g.add_edge(1, 7, 11);
+    // g.add_edge(2, 3, 7);
+    // g.add_edge(2, 8, 2);
+    // g.add_edge(2, 5, 4);
+    // g.add_edge(3, 4, 9);
+    // g.add_edge(3, 5, 14);
+    // g.add_edge(4, 5, 10);
+    // g.add_edge(5, 6, 2);
+    // g.add_edge(6, 7, 1);
+    // g.add_edge(7, 8, 7);
+    // g.add_edge(6, 8, 6);
+    // g.set_destination(3);
+    // g.dijsktra();
     // g.print();
+    int n, m; //n->no of nodes, m->no of edges;
+    cin>>n>>m;
+    Graph g(n);
+    int node1, node2, weight;
+    for(int i=0; i<m; i++){
+        cin>>node1>>node2>>weight;
+        g.add_edge(node1, node2, weight);
+    }
+    int destination_node;
+    cin>>destination_node;
+    g.set_destination(destination_node);
+    g.dijsktra();
     return 0;
 }
